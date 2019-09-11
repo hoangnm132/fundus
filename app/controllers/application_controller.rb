55ca45c76b030f523b93ce66e2a9a5ba85ff0a7e
Hoang Nguyen Minh <Raptor@Hoangs-MacBook-Pro.local>
2019-09-11 17:06:00 +0700

    commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+
+  include SessionsHelper
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.root_path, alert: exception.message
+  end
+
+  protect_from_forgery with: :exception
+end
